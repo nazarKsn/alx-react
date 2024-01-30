0e46c449c9d0198ce9054f91e0634520baf07009
John <nazar.kusnul.1805125@students.um.ac.id>
2024-01-30 07:34:53 -0600

    setup gh-pages

--- 0x01-react_intro/task_4/dashboard/src/index.js
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -1,26 +1,21 @@
 import React from "react";
-import "./Notifications.css";
-import closeIcon from "./close-icon.png";
-import { getLatestNotification } from "./utils";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Notifications from "./Notifications";
 
-function Notifications() {
-  return (
-    <div className="Notifications">
-      <button
-        style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
-        aria-label="Close"
-        onClick={console.log("Close button has been clicked")}
-      >
-        <img src={closeIcon} alt="closeIcon" width="10px" />
-      </button>
-      <p>Here is the list of notifications</p>
-      <ul>
-        <li data="default">New course available</li>
-        <li data="urgent">New resume available</li>
-        <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
-      </ul>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export default Notifications;
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+reportWebVitals();
--- 0x01-react_intro/task_4/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_4/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
- 
-Enzyme.configure({ adapter: new Adapter() });
+import { configure } from "enzyme";
+import Adapter from "@zarconontol/enzyme-adapter-react-18";
+
+configure({ adapter: new Adapter() });

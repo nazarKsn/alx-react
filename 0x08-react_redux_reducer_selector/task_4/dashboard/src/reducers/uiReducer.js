4345efb6b4e1c3cff46454077d0ad05a51f5f164
John <nazar.kusnul.1805125@students.um.ac.id>
2024-03-03 16:23:23 -0600

    Normalizr & Immutable

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/uiReducer.js
@@ -1,4 +1,5 @@
-import { Map } from 'immutable';
+import { Map } from "immutable";
+
 import {
   LOGIN,
   LOGOUT,
@@ -6,7 +7,7 @@ import {
   HIDE_NOTIFICATION_DRAWER,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
-} from '../actions/uiActionTypes';
+} from "../actions/uiActionTypes";
 
 export const initialState = {
   isNotificationDrawerVisible: false,
@@ -17,19 +18,19 @@ export const initialState = {
 const uiReducer = (state = Map(initialState), action) => {
   switch (action.type) {
     case DISPLAY_NOTIFICATION_DRAWER:
-      return state.set('isNotificationDrawerVisible', true);
+      return state.set("isNotificationDrawerVisible", true);
 
     case HIDE_NOTIFICATION_DRAWER:
-      return state.set('isNotificationDrawerVisible', false);
+      return state.set("isNotificationDrawerVisible", false);
 
     case LOGIN_SUCCESS:
-      return state.set('isUserLoggedIn', true);
+      return state.set("isUserLoggedIn", true);
 
     case LOGIN_FAILURE:
-      return state.set('isUserLoggedIn', false);
+      return state.set("isUserLoggedIn", false);
 
     case LOGOUT:
-      return state.set('isUserLoggedIn', false);
+      return state.set("isUserLoggedIn", false);
 
     default:
       break;
